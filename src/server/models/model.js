aba8ebe182e4ee3cc0f6e1bcc7cdca733c9810d3
Charles Pascoe <cpascoe95@gmail.com>
2016-10-08 12:54:43 +0100

    Changed the way model schemas are defined

--- src/server/models/model.js
+++ src/server/models/model.js
@@ -2,7 +2,7 @@ import Utils from '../utils';
 import SqlUtils from '../database/sql-utils';
 
 export default class Model {
-  constructor(database, loggers, tableName, idColumns, data) {
+  constructor(database, tableName, idColumns, data) {
     if (typeof idColumns == 'string') {
       idColumns = [idColumns];
     }
@@ -10,25 +10,36 @@ export default class Model {
     this._tableName = tableName;
     this._idColumns = idColumns;
     this._database = database;
-    this._loggers = loggers;
 
     // Need to clone!
     this._previousData = Utils.copy(data);
     this._data = Utils.copy(data);
   }
 
-  defineProperty(propertyName, columnName) {
-    if (this._idColumns.find(columnName)) {
-      Object.defineProperty(this, propertyName, {
-        value: this._data[columnName]
-      });
-      return;
-    }
+  defineSchema(schema) {
+    for (var column in schema) {
+      if (this._data.hasOwnProperty(column)) {
+        throw new Error(`Data element expected: column ${column} not found on data for ${this._tableName}`);
+      }
 
-    Object.defineProperty(this, propertyName, {
-      get: () => this._data[columnName],
-      set: (value) => this._data[columnName] = value
-    });
+      (function (col, meta) {
+        if (meta.readonly) {
+          Object.defineProperty(this, meta.property || col, {
+            value: this._data[col]
+          });
+          return;
+        }
+
+        Object.defineProperty(this, meta.property || col, {
+          get: () => this._data[col],
+          set: (value) => {
+            // Type checking to go here
+            this._data=  value;
+          }
+        });
+
+      })(column, schema[column]);
+    }
   }
 
   async save() {
@@ -52,15 +63,23 @@ export default class Model {
     var query =
       `UPDATE \`${SqlUtils.removeInvalidChars(this._tableName)}\` ` +
         `SET ${SqlUtils.formatData(changedFields)} ` +
-        `WHERE ${SqlUtils.formatData(ids)};`
+        `WHERE ${SqlUtils.formatData(ids, null ,' AND ')};`
 
-    try {
-      await this.database.query(query, this._data);
-    } catch (e) {
-      this._loggers.database.error({err: e, query: query}, `Failed to save ${this._tableName}`);
-      throw e;
-    }
+    await this.database.query(query, this._data);
 
     this._previousData = Utils.copy(this._data);
+  }
+
+  async delete() {
+    var ids = {};
+
+    for (var idCol of this._idColumns) {
+      ids[idCol] = this._data[idCol];
+    }
+
+    var query =
+      `DELETE FROM \`${SqlUtils.removeInvalidChars(this._tableName)}\` WHERE ${SqlUtils.formatData(ids, null, ' AND ')};`;
+
+    await this.database.query(query, ids);
   }
 }
