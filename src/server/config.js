5c1f5865a109a50e71f241ac95ffa5e21d93398d
Charles Pascoe <cpascoe95@gmail.com>
2016-10-08 17:50:48 +0100

    Logging level now configurable

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,18 @@
 import configuration from './configuration';
+import loggers from './loggers';
 
 // Add other things to config here e.g. default values/env variables
+
+// Production mode
+if (typeof configuration.production != 'boolean') {
+  configuration.production = (process.env['NODE_ENV'] == 'production');
+}
+
+// Configure logging
+if (configuration.logging) {
+  for (var logger in loggers) {
+    loggers[logger].level(bunyan[configuration.logging.level] || bunyan.INFO);
+  }
+}
 
 export default configuration;
