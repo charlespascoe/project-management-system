ba26d986adcb921b488b10bac084080e1dd83d3a
Charles Pascoe <cpascoe95@gmail.com>
2016-10-08 18:23:42 +0100

    Rewrote Utils.promisify to add promises to an entire object

--- src/server/utils.js
+++ src/server/utils.js
@@ -3,16 +3,33 @@ export default class Utils {
     return Object.assign({}, obj);
   }
 
-  static promisify(func) {
-    return function (...args) {
-      return new Promise(function (fulfill, reject) {
-        args.push(function (err, result) {
-          if (err) return reject(err);
-          fulfill(result);
-        });
+  static promisify(obj) {
+    // If noPromise defined, assume already promisified
+    if (obj.noPromise) return;
 
-        func.apply(undefined, args);
-      });
+    obj.noPromise = {};
+
+    for (var memberName in obj) {
+      if (typeof obj[memberName] != 'function') continue;
+
+      (function (funcName) {
+        var prevFunc = obj[funcName];
+
+        obj.noPromise = prevFunc;
+
+        obj[funcName] = function (...args) {
+          return new Promise(function (fulfill, reject) {
+            args.push(function (err, result) {
+              if (err) return reject(err);
+              fulfill(result);
+            });
+
+            prevFunc.apply(obj, args);
+          });
+        };
+      })(memberName);
     }
+
+    return obj;
   }
 }
