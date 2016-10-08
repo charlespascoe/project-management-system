109e8b498956dfe56e5c0de1b941428c7a94b120
Charles Pascoe <cpascoe95@gmail.com>
2016-10-08 18:31:48 +0100

    loggers now loads config directly, added src to config

--- src/server/config.js
+++ src/server/config.js
@@ -1,19 +1,10 @@
 import configuration from './configuration';
-import bunyan from 'bunyan';
-import loggers from './loggers';
 
 // Add other things to config here e.g. default values/env variables
 
 // Production mode
 if (typeof configuration.production != 'boolean') {
   configuration.production = (process.env['NODE_ENV'] == 'production');
-}
-
-// Configure logging
-if (configuration.logging) {
-  for (var logger in loggers) {
-    loggers[logger].level(bunyan[configuration.logging.level] || bunyan.INFO);
-  }
 }
 
 export default configuration;
--- src/server/loggers.js
+++ src/server/loggers.js
@@ -1,8 +1,11 @@
 import bunyan from 'bunyan';
+import config from './config';
 
 export default {
   db: bunyan.createLogger({
     name: 'db',
-    serializers: { err: bunyan.stdSerializers.err }
+    serializers: { err: bunyan.stdSerializers.err },
+    level: bunyan[config.logging.level] || bunyan.INFO,
+    src: config.logging.src
   })
 };
