c730a6568caeac3597249035fe1d052d03a43860
Charles Pascoe <cpascoe95@gmail.com>
2016-10-08 19:31:53 +0100

    Added Utils.defaults

--- src/server/utils.js
+++ src/server/utils.js
@@ -1,4 +1,8 @@
 export default class Utils {
+  static defaults(defaults, additional) {
+    return Object.assign(Utils.copy(defaults), additional);
+  }
+
   static copy(obj) {
     return Object.assign({}, obj);
   }
