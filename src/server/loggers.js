90f1cee85d9975dd6a9c9de3467804894f34c30f
Charles Pascoe <cpascoe95@gmail.com>
2016-10-08 19:32:06 +0100

    Added main logger

--- src/server/loggers.js
+++ src/server/loggers.js
@@ -1,11 +1,18 @@
 import bunyan from 'bunyan';
 import config from './config';
+import Utils from './utils';
+
+const commonConfig = {
+  serializers: { err: bunyan.stdSerializers.err },
+  level: bunyan[config.logging.level] || bunyan.INFO,
+  src: config.logging.src
+}
 
 export default {
-  db: bunyan.createLogger({
-    name: 'db',
-    serializers: { err: bunyan.stdSerializers.err },
-    level: bunyan[config.logging.level] || bunyan.INFO,
-    src: config.logging.src
-  })
+  main: bunyan.createLogger(Utils.defaults(commonConfig, {
+    name: 'main'
+  })),
+  db: bunyan.createLogger(Utils.defaults(commonConfig, {
+    name: 'db'
+  }))
 };
