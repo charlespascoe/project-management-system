b088c3329cdb6768da052fac385d33f9a4ed00ca
Charles Pascoe <cpascoe95@gmail.com>
2016-10-09 17:59:51 +0100

    Removed UserManager, and moved its functionality into User class as static methods

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,5 +1,8 @@
 import Model from './model';
 import Schema from './schema';
+import AuthenticationToken from './authentication-token';
+import database from '../database/database';
+import SqlUtils from '../database/sql-utils';
 
 export default class User extends Model {
   constructor(database, data) {
@@ -11,6 +14,53 @@ export default class User extends Model {
   async delete() {
     this.active = false;
     await this.save();
+  }
+
+  // Static methods
+
+  static async getUserByEmail(email) {
+    var userQuery =
+      'SELECT * FROM `user` WHERE `email` = :email;';
+
+    var userResult = await User.database.queryForOne(userQuery, {email: email});
+
+    if (userResult == null) return null;
+
+    var authTokensQuery =
+      'SELECT * FROM `authentication_token` WHERE `user_id` = :user_id;';
+
+    userResult.authTokens = await User.database.query(authTokensQuery, {user_id: userResult.user_id});
+
+    return new User(User.database, userResult);
+  }
+
+  static async getUserById(userId) {
+    var userQuery =
+      'SELECT * FROM `user` WHERE `user_id` = :user_id; ' +
+      'SELECT * FROM `authentication_token` WHERE `user_id` = :user_id;';
+
+    var results = await User.database.query(userQuery, {user_id: userId}),
+        userResult = results[0][0],
+        authTokensResult = results[1];
+
+    if (userResult == null) return null;
+
+    userResult.authTokens = authTokensResult.map((row) => new AuthenticationToken(User.database, row));
+
+    return new User(User.database, userResult);
+  }
+
+  static async addUser(data) {
+    var columnData = User.schema.mapPropertiesToColumns(data);
+
+    if (columnData == null) throw new Error('Invalid data provided to UserManager.addUser');
+
+    var query =
+      'INSERT INTO `user` SET ' + SqlUtils.formatData(columnData) + ';';
+
+    var result = await User.database.query(query, columnData);
+
+    return result.insertId;
   }
 }
 
@@ -36,3 +86,5 @@ User.schema = new Schema({
     column: 'active'
   }
 });
+
+User.database = database;
