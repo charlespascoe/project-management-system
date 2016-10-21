40f22cf0386a5a17b819a87d541c72fa291c89ac
Charles Pascoe <cpascoe95@gmail.com>
2016-10-21 17:38:53 +0100

    AuthenticationController.login now validates username and password

--- src/server/controllers/authentication-controller.js
+++ src/server/controllers/authentication-controller.js
@@ -1,6 +1,8 @@
 import authenticator from 'server/security/authenticator';
 import loggers from 'server/loggers';
 import Result from 'server/controllers/result';
+import validate from 'server/validation';
+import User from 'server/models/user';
 
 export class AuthenticationController {
   constructor(authenticator, loggers) {
@@ -10,6 +12,12 @@ export class AuthenticationController {
 
   async login(username, password) {
     var result = new Result();
+
+    if (!User.schema.email.validate(username) ||
+        !validate(password).isString().minLength(1).maxLength(1024).isValid()) {
+      this.loggers.security.warn('Invalid login request');
+      return result.delay().status(400);
+    }
 
     var authTokenPair = await this.authenticator.login(username, password);
 
--- tests/controllers/authentication-controller-tests.js
+++ tests/controllers/authentication-controller-tests.js
@@ -4,8 +4,6 @@ import { AuthenticationController } from 'server/controllers/authentication-cont
 import { validation } from 'server/validation';
 import dummyLoggers from 'tests/dummy-loggers';
 
-const validate = validation().check;
-
 const catchHandler = catchAsync(function (err, st) {
   st.fail('Unexpected exception: ' + err.toString());
   st.end();
@@ -22,6 +20,24 @@ tests.createInstance = function () {
 };
 
 tests.testMethod('login', function (t) {
+  t.test('It should return 400 for undefined username', catchHandler(async function (st, authController) {
+    authController.authenticator.login = async () => st.fail('Authenticator.login should not have been called');
+
+    var result = await authController.login(undefined, 'pass1234');
+    st.equals(result.changes.status, 400);
+    st.ok(result.changes.delay > 0);
+    st.end();
+  }));
+
+  t.test('It should return 400 for invalid username', catchHandler(async function (st, authController) {
+    authController.authenticator.login = async () => st.fail('Authenticator.login should not have been called');
+
+    var result = await authController.login('a'.repeat(2048), 'pass1234');
+    st.equals(result.changes.status, 400);
+    st.ok(result.changes.delay > 0);
+    st.end();
+  }));
+
   t.test('It should return 401 for invalid user login', catchHandler(async function (st, authController) {
     var result = await authController.login('bob', 'pass1234');
     st.equals(result.changes.status, 401);
