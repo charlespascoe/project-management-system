02f66a02d8718b738e5290255c50a2a271cf2204
Charles Pascoe <cpascoe95@gmail.com>
2016-11-01 14:49:24 +0000

    Modified config to contain default and logs config file from env variable

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,36 @@
-import configuration from 'server/configuration';
+import fs from 'fs';
 
-// Add other things to config here e.g. default values/env variables
+const CONFIG_PATH = process.env['CONFIG_PATH'] || __dirname + '/configuration.json';
+
+var configuration;
+
+try {
+  configuration = JSON.parse(fs.readFileSync(CONFIG_PATH));
+} catch (e) {
+  console.error(e);
+  console.log(`Configuration file (${CONFIG_PATH}) not accessible or not valid JSON - using defaults`);
+  configuration = {};
+}
+
+// Database
+if (!configuration.db) {
+  configuration.db = {
+    connectionLimit: 10,
+    host: 'localhost',
+    database: 'prog_mgr'
+  };
+}
+
+// Logging
+if (!configuration.logging) {
+  configuration.logging = {
+    level: 'INFO',
+    src: false
+  };
+
+}
+
+configuration.logging.logOutputDir = process.env['LOG_DIR'] || configuration.logging.logOutputDir;
 
 // Production mode
 if (typeof configuration.production != 'boolean') {
