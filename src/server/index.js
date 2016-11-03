863a2b34818c65a869d0d767c500dddd7de6b08f
Charles Pascoe <cpascoe95@gmail.com>
2016-11-03 11:35:40 +0000

    Changes to server index file

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,14 @@ import database from 'server/database/database';
 import loggers from 'server/loggers';
 import users from 'server/database/users';
 import express from 'express';
-import rootRouter from 'server/routers/root';
-import authRouter from 'server/routers/auth-router';
 import bodyParser from 'body-parser';
 import Result from 'server/controllers/result';
+import config from 'server/config';
+import routers from 'server/routers';
+import authenticate from 'server/controllers/authenticate';
 
 const app = express();
+const port = 8080;
 
 app.locals.appName = 'Jeera';
 app.locals.version = '0.0.0-DEV';
@@ -17,12 +19,27 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views/pages');
 
 app.use(function (req, res, next) {
-  res.result = new Result();
+  res.result = new Result(res);
+
+  if (config.xForwardedFor) {
+    req.ip = req.headers['x-forwarded-for'] || req.ip;
+  }
+
   next();
 });
+
 app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use('/', rootRouter);
-app.use('/auth/', authRouter);
 
-app.listen(8080, (e) => e ? loggers.main.fatal({err: e}) : loggers.main.info('Listening on port 8080'));
+routers(app);
+
+app.get('/something', authenticate, function (req, res) {
+  res.status(200).json({result: true});
+});
+
+app.use(function (req, res) {
+  loggers.main.warn({ip: req.ip}, `Route not found: ${req.path}`);
+  res.result.delay().status(404).end();
+});
+
+app.listen(port, (e) => e ? loggers.main.fatal({err: e}) : loggers.main.info(`Listening on port ${port}`));
