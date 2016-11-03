46a03b95e3ac74105e57ec1a3f4f1612b7c510b8
Charles Pascoe <cpascoe95@gmail.com>
2016-11-03 19:43:07 +0000

    Added Project schema ID validation and Project.serialise()

--- src/server/models/project.js
+++ src/server/models/project.js
@@ -2,16 +2,24 @@ import Model from 'server/models/model';
 import Schema from 'server/models/schema';
 import validate from 'server/validation';
 
-export class Project extends Model {
+export default class Project extends Model {
   constructor(database, data) {
     super(database, 'project', data, Project.schema);
+  }
+
+  serialise() {
+    return {
+      id: this.id,
+      name: this.name
+    }
   }
 }
 
 Project.schema = new Schema({
   id: {
     column: 'project_id',
-    id: true
+    id: true,
+    validate: (val) => validate(val).isString().matches(/^[A-Z]{1,16}$/).isValid()
   },
   name: {
     column: 'project_name',
