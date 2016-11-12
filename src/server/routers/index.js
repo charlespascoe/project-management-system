3cfab44e25614eb9f2193cc0065d0931e36bdc3d
Charles Pascoe <cpascoe95@gmail.com>
2016-11-12 09:54:03 +0000

    Removed root router and mounted the users router

--- src/server/routers/index.js
+++ src/server/routers/index.js
@@ -1,10 +1,10 @@
-import authenticate from 'server/middleware/authenticate';
-import rootRouter from 'server/routers/root';
 import authRouter from 'server/routers/auth';
+import authenticate from 'server/middleware/authenticate';
 import projectsRouter from 'server/routers/projects';
+import usersRouter from 'server/routers/users';
 
 export default function routers(app) {
-  app.use('/', rootRouter);
   app.use('/auth', authRouter);
   app.use('/projects', authenticate, projectsRouter);
+  app.use('/users', authenticate, usersRouter);
 };
