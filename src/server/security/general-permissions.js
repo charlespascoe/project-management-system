c95308e2d2781ec72b385a1a4c8b39f3e4188f2c
Charles Pascoe <cpascoe95@gmail.com>
2016-11-12 09:10:39 +0000

    Added start of general permissions

---
+++ src/server/security/general-permissions.js
@@ -0,0 +1,12 @@
+const generalPermissions = {
+  GET_OTHER_USER_DETAILS: {
+    sysadmin: true
+  }
+};
+
+export default new Proxy(generalPermissions, {
+  get: (permissions, permKey) => {
+    if (permKey in permissions) return permissions[permKey];
+    throw new Error(`Can't find permission with key '${permKey}'`);
+  }
+});
