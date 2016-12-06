c5e85f8e727b656432f5ad5a0320ca89fa1bbff4
Charles Pascoe <cpascoe95@gmail.com>
2016-12-06 09:14:29 +0000

    Updated members router to use MembersController.updateMember and removeMember

--- src/server/routers/projects/members.js
+++ src/server/routers/projects/members.js
@@ -3,6 +3,7 @@ import catchAsync from 'server/catch-async';
 import httpStatuses from 'http-status-codes';
 import loggers from 'server/loggers';
 import membersController from 'server/controllers/members-controller';
+import User from 'server/models/user';
 
 const catchHandler = catchAsync(function (err, req, res) {
   loggers.main.error({err: err});
@@ -22,18 +23,26 @@ router.route('/')
   }));
 
 router.param('userId', function (req, res, next) {
-  if (!validate(req.params.userId).matches(/^\d+$/).isValid()) {
+  if (!User.schema.id.validate(req.params.userId)) {
     res.result.delay().status(httpStatuses.BAD_REQUEST).end();
+    loggers.main.debug({user: req.user}, `Invalid User ID: ${req.params.userId}`);
     return;
   }
 
-  req.params.userId = parseInt(req.params.userId);
+  req.userId = parseInt(req.params.userId);
 
   next();
 });
 
-router.delete('/:userId', catchHandler(async function (req, res) {
-  // Remove a member
-}));
+router.route('/:userId')
+  .put(catchHandler(async function (req, res) {
+    await membersController.updateMember(res.result, req.user, req.projectId, req.userId, req.body);
+    await res.result.end();
+  }))
+  .delete(catchHandler(async function (req, res) {
+    await membersController.removeMember(res.result, req.user, req.projectId, req.userId);
+    await res.result.end();
+  }));
+
 
 export default router;
