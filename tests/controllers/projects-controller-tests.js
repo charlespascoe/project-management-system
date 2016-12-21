6138c066ba0074145ac041232458f0d9cdb26180
Charles Pascoe <cpascoe95@gmail.com>
2016-12-21 14:18:44 +0000

    ProjectsController: Fix createProject tests
    Fix ProjectsController.createProject tests to match new Controller
    style, add missing Icon URL parameter

--- tests/controllers/projects-controller-tests.js
+++ tests/controllers/projects-controller-tests.js
@@ -10,25 +10,30 @@ const catchHandler = catchAsync(function (err, st) {
 });
 
 
-const tests = new TestFrame('AuthenticationController');
+const tests = new TestFrame('ProjectsController');
 tests.createInstance = function () {
   var projects = {
-    createProjectResult: false,
-    createProject: async () => {
-      return projects.createProjectResult;
-    }
+    createProject: async () => false
   };
 
-  return new ProjectsController(dummyLoggers, projects);
+  var authorisor = {
+    hasGeneralPermission: () => true
+  };
+
+  return new ProjectsController(dummyLoggers, projects, authorisor);
 };
 
 tests.testMethod('createProject', function (t) {
   t.test('It should return 400 for invalid project name', catchHandler(async function (st, projController) {
     projController.projects.createProject = () => st.fail('Projects.createProject should not have been called');
-    var result = await projController.createProject(new Result(), {}, {
+    var result = new Result();
+
+    await projController.createProject(result, {}, {
       id: 'ABC',
-      name: 'a'.repeat(1000)
+      name: 'a'.repeat(1000),
+      iconUrl: 'https://www.example.com/icon.png'
     });
+
     st.equals(result.changes.status, 400);
     st.ok(result.changes.delay > 0);
     st.end();
@@ -36,22 +41,45 @@ tests.testMethod('createProject', function (t) {
 
   t.test('It should return 400 for invalid project ID', catchHandler(async function (st, projController) {
     projController.projects.createProject = () => st.fail('Projects.createProject should not have been called');
-    var result = await projController.createProject(new Result(), {}, {
+    var result = new Result();
+
+    await projController.createProject(result, {}, {
       id: '?? !123',
-      name: 'Example'
+      name: 'Example',
+      iconUrl: 'https://www.example.com/icon.png'
     });
+
+    st.equals(result.changes.status, 400);
+    st.ok(result.changes.delay > 0);
+    st.end();
+  }));
+
+  t.test('It should return 400 for invalid icon URL', catchHandler(async function (st, projController) {
+    projController.projects.createProject = () => st.fail('Projects.createProject should not have been called');
+    var result = new Result();
+
+    await projController.createProject(result, {}, {
+      id: 'TEST',
+      name: 'Example',
+      iconUrl: 'Clearly not a URL'
+    });
+
     st.equals(result.changes.status, 400);
     st.ok(result.changes.delay > 0);
     st.end();
   }));
 
   t.test('It should return 409 for duplicate ID', catchHandler(async function (st, projController) {
-    projController.projects.createProjectResult = true;
+    projController.projects.createProject = async () => true;
 
-    var result = await projController.createProject(new Result(), {}, {
+    var result = new Result();
+
+    await projController.createProject(result, {}, {
       id: 'TEST',
-      name: 'Test Project'
+      name: 'Test Project',
+      iconUrl: 'https://www.example.com/icon.png'
     });
+
     st.equals(result.changes.status, 409);
     // No delay, which is fine
     st.end();
