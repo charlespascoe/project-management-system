01200fd265f6b41f3d4fc911442ff17dd8b25944
Charles Pascoe <cpascoe95@gmail.com>
2017-01-10 15:04:52 +0000

    Testing: Add Projects tests

---
+++ tests/database/projects-tests.js
@@ -0,0 +1,65 @@
+import { Projects } from 'server/database/projects';
+import TestFrame from 'tests/test-frame';
+import dummyDatabase from 'tests/dummy-database';
+
+const projects = new Projects(dummyDatabase);
+
+const tests = new TestFrame('Projects');
+tests.createInstance = () => projects;
+
+tests.before = async () => {
+  dummyDatabase.init();
+  await dummyDatabase.reset();
+};
+
+tests.after = async (st) => {
+  await dummyDatabase.end();
+  st.end();
+};
+
+tests.testMethod('getAllProjects', function (t) {
+  t.test('It should return all projects', async function (st, projects) {
+    var projects = await projects.getAllProjects();
+
+    st.equals(projects.length, 1);
+    st.equals(projects[0].id, 'EXAMPLE');
+    st.equals(projects[0].name, 'Example Project');
+    st.equals(projects[0].iconUrl, 'https://www.example.com/icon.png');
+  });
+});
+
+tests.testMethod('getProject', function (t) {
+  t.test('It should return null for non-existent project ID', async function (st, projects) {
+    var project = await projects.getProject('FAKEID');
+    st.equals(project, null);
+  });
+
+  t.test('It should return the project for a real project ID', async function (st, projects) {
+    var project = await projects.getProject('EXAMPLE');
+    st.equals(project.id, 'EXAMPLE');
+    st.equals(project.name, 'Example Project');
+    st.equals(project.iconUrl, 'https://www.example.com/icon.png');
+  });
+});
+
+tests.testMethod('createProject', function (t) {
+  t.test('It should return true for a duplicate project ID', async function (st, projects) {
+    var idExists = await projects.createProject({
+      id: 'EXAMPLE',
+      name: 'Existing Project',
+      iconUrl: 'https://www.example.com/icon.png'
+    });
+
+    st.equals(idExists, true);
+  });
+
+  t.test('It should return false after successfully creating a project', async function (st, projects) {
+    var idExists = await projects.createProject({
+      id: 'NEWID',
+      name: 'New Project',
+      iconUrl: 'https://www.example.com/icon.png'
+    });
+
+    st.equals(idExists, false);
+  });
+});
